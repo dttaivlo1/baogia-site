53bb00b35a40508e0e6828a19287ff89616a9bc6
dttaivlo1 <dttaivlo@gmail.com>
2023-07-31 15:10:46 +0700

    a
    a

--- src/app/core/configs/constants/api-command.constant.ts
+++ src/app/core/configs/constants/api-command.constant.ts
@@ -1,6 +1,6 @@
 export const BaseURL = {
-  BASE_API_URL: 'https://api-hth.wittyocean-5088eb6c.eastasia.azurecontainerapps.io/api/v1',
-  //BASE_API_URL: 'http://localhost:8800/api/v1',
+ // BASE_API_URL: 'https://api-hth.wittyocean-5088eb6c.eastasia.azurecontainerapps.io/api/v1',
+  BASE_API_URL: 'http://localhost:8800/api/v1',
   API_REPORT: '/api/report',
   API_EMAIL: '/api/emails',
   API_USER:'/users',
